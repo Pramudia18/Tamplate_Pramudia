a3ceded1c218bbf4ba772904166030210807844a
Pramudiapermana78@gmail.com <pramudiapermana78@mail.com>
2021-10-12 13:06:06 +0700

    .

---
+++ application/views/v_profile.php
@@ -0,0 +1,9 @@
+<section>
+    <h2>Halaman Profil</h2>
+    <p>Haaaii salam kenal semuaa</p> <br>
+    <p>Nama : Pramudia Permana</p> <br>
+    <p>NIM : 12200006</p> <br>
+    <p>Kelas : 12.3A.03</p> <br>
+    <p>Jurusan : Sistem Informasi</p><br>
+
+</section>
